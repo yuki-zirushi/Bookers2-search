5cd3b33c0281138e7ad43e6beabe1f2c362570a8
yuki-zirushi <yuko.12.151@gmail.com>
2021-06-06 23:36:00 +0000

    [Add] 検索方法の条件分岐

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,22 @@ class User < ApplicationRecord
          has_many :books, dependent: :destroy
 
   attachment :profile_image
-  
+
   validates :name, presence: true, length: { in: 2..20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
-  
+
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @user = User.where("name LIKE?", "#{word}")
+    elsif search == "forward_match"
+      @user = User.where("name LIKE?", "#{word}%")
+    elsif search == "backword_match"
+      @user = User.where("name LIKE?", "%#{word}")
+    elsif search == "partial_match"
+      @user = User.where("name LIKE", "%#{word}%")
+    else
+      @user = User.all
+    end
+  end
+
 end
