49f6efd4c2e89d457f6bb8f15d831707903b25d8
yuki-zirushi <yuko.12.151@gmail.com>
2021-06-07 02:53:56 +0000

    [Add] 解答

---
+++ search-master/app/models/book.rb
@@ -0,0 +1,17 @@
+class Book < ApplicationRecord
+  belongs_to :user
+  validates :title, presence: true
+  validates :body, presence: true, length: { maximum: 200 }
+
+  def self.search_for(content, method)
+    if method == 'perfect'
+      Book.where(title: content)
+    elsif method == 'forward'
+      Book.where('title LIKE ?', content+'%')
+    elsif method == 'backward'
+      Book.where('title LIKE ?', '%'+content)
+    else
+      Book.where('title LIKE ?', '%'+content+'%')
+    end
+  end
+end
---
+++ search-master/app/models/user.rb
@@ -0,0 +1,24 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :books
+  attachment :profile_image, destroy: false
+
+  validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
+  validates :introduction, length: { maximum: 50 }
+
+  def self.search_for(content, method)
+    if method == 'perfect'
+      User.where(name: content)
+    elsif method == 'forward'
+      User.where('name LIKE ?', content + '%')
+    elsif method == 'backward'
+      User.where('name LIKE ?', '%' + content)
+    else
+      User.where('name LIKE ?', '%' + content + '%')
+    end
+  end
+end
